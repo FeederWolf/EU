aa2bee358491c71b747cf3eaf60900e28f11659e
FeederWolf <Kollarg22@gmail.com>
2022-01-18 10:20:55 +0100

    Add project files.

---
+++ EU/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Dynamic;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
